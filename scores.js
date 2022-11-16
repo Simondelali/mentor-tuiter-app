e784555881745bc26c6ee3fb87d337436b6510c8
Simon Atiegar <atiegardelali@gmail.com>
2022-11-16 23:42:11 +0000

    update scores

--- scores.js
+++ scores.js
@@ -1,6 +1,31 @@
-let score = [69,84,54,73,53,67,87]
-score.sort()
-console.log(score)
-score.shift();
-score.shift();
-console.log(score)
+'use strict';
+
+const score = [69,84,54,73,53,67,87];
+const betterScore = [... score];
+
+// sort and delete first 2 lowest 
+betterScore.sort();
+betterScore.shift();
+betterScore.shift();
+
+//compute average
+let avg = 0;
+for (const s of betterScore){
+    avg +=s;
+}
+avg /= betterScore.length;
+
+//round avg
+avg = Math.round(avg);
+
+//append avg to betterScore
+betterScore.push(avg);
+betterScore.push(avg);
+ 
+console.log(score);
+console.log(betterScore);
+
+
+
+
+
