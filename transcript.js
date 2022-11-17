b4eba6f2bf18d754a18f3459cf040b40bc3cc61a
Simon Atiegar <atiegardelali@gmail.com>
2022-11-17 16:07:15 +0000

    Update constructor

--- transcript.js
+++ transcript.js
@@ -8,6 +8,33 @@ function exam (code, name, credits, score, date){
     this.date = date;
 }
 
-//function examList()
-var exam1 = new exam('CE12', 'Ceng', 3 , 80 , 12);
-console.log(exam1.code)
+
+//var exam1 = new exam('CE12', 'Ceng', 3 , 80 , '12-08-22');
+//console.log(exam1.code)
+
+function examList(){
+    this.list = [];
+
+    this.add = (e) => {
+        this.list.push(e);
+    }
+
+    this.find = (code) => {
+        for (const c of this.list)
+        if (c.code === code)
+        return c;
+        return undefined;
+    }
+
+}
+const exams1 = new exam('CE12', 'Computational Engineering', 3 , 78 , '12-08-22');
+const exams2 = new exam('FC12', 'French', 2 , 86 , '22-08-22');
+const exams3 = new exam('ME12', 'Thermodynamics', 3 , 80 , '03-08-22');
+
+const exams = new examList();
+exams.add(exams1);
+exams.add(exams2);
+exams.add(exams3);
+
+console.log("FIND COURSE");
+console.log(exams.find('ME12'));
