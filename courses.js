55e100c533f785ec7476377cd96fcfdcf843f9f2
Simon Atiegar <atiegardelali@gmail.com>
2022-11-17 01:30:38 +0000

    update courses

--- courses.js
+++ courses.js
@@ -4,3 +4,30 @@ const courseName = `Data Structures and Algorithm, Programming and problem solvi
 MATLAB, Communication systems, Principles of Accounting, Digital Systems, 
 Discrete Structures, Algebra, Calculus`
 
+//console.log(courseName)
+
+//Join multi-line string
+const modif = courseName.replace(/\n/g, '');
+
+//create courses array
+const courses = modif.split(',');
+//console.log(courses);
+
+//clean whitespace around commas
+for (let i = 0; i < courses.length; i++){
+    courses[i] = courses[i].trim();
+}
+
+// create acronyms
+const acronyms = [];
+for (const c of courses){
+    const words = c.split('');
+    let acronym = '';
+    for (let w of words){
+        acronym += w[0].toUpperCase();
+    }
+    acronyms.push(acronym);
+}
+//prints acronyms + course name
+
+
