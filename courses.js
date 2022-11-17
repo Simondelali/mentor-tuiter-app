7980391a834c4c5c06df3a4e987c74f0a692a074
Simon Atiegar <atiegardelali@gmail.com>
2022-11-17 00:16:50 +0000

    update

--- courses.js
+++ courses.js
@@ -1,1 +1,6 @@
 'use strict';
+
+const courseName = `Data Structures and Algorithm, Programming and problem solving,
+MATLAB, Communication systems, Principles of Accounting, Digital Systems, 
+Discrete Structures, Algebra, Calculus`
+
